fc5667ffba75cc4a5999fe62025b5f9a348fb109
mattl <matt.leith@outlook.com>
2021-06-28 18:42:32 +1000

    swagger config to hide default swagger entry

--- src/main/java/com/codetest/toyrobot/ToyrobotApplication.java
+++ src/main/java/com/codetest/toyrobot/ToyrobotApplication.java
@@ -2,6 +2,11 @@ package com.codetest.toyrobot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class ToyrobotApplication {
@@ -10,4 +15,13 @@ public class ToyrobotApplication {
 		SpringApplication.run(ToyrobotApplication.class, args);
 	}
 
+	@Bean
+	public Docket swaggerConfig() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage(this.getClass().getPackageName()))
+				.paths(PathSelectors.any())
+				.build()
+				.useDefaultResponseMessages(false);
+	}
 }
